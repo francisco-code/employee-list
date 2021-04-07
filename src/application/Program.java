be0cc1b489a32066e8a39eac0fa1691256646fca
Felipe Correia <felipedreamcorreia@hotmail.com>
2021-04-07 19:29:47 -0300

    feature: programa de funcionários

---
+++ src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		
+		System.out.print("How many employees will be registered? ");
+		int N = sc.nextInt();
+		System.out.println();
+		
+		for (int i=0; i<N; i++) {
+			
+			System.out.println("Employee #" + (i+1) + ":");
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			
+			Employee emp = new Employee(id, name, salary);
+			
+			list.add(emp);
+			System.out.println();
+		}
+		
+		System.out.print("Enter the employee id that will have salary increase: ");
+		
+		
+		
+
+		sc.close();
+	}
+
+}
